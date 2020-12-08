2188e64a29ef900b04d5b97f6a2f1076aff571cd
nnekasandra <nnekasandra2016@gmai>
2020-12-08 21:49:27 +0100

    challenge now solved but stored to an array instead of string

--- scripts.js
+++ scripts.js
@@ -3,9 +3,9 @@
           /*function that takes an array of numbers containing two elements sorted and returns any numbers that occur in both elements*/
           let elem1 = strArr[0]; //elem1 variable holds the first element of strArr array
           let elem2 = strArr[1]; //elem2 variable holds the second element of strArr array
-          elem1 = elem1.split(',') //converts the first elements of strArr to string items
-          elem2 = elem2.split(',') //converts the second elements of strArr to string items
-          console.log(elem2)
+          elem1 = elem1.split(',').sort() //converts the first elements of strArr to string items
+          elem2 = elem2.split(',').sort() //converts the second elements of strArr to string items
+          let intersectingNumbers = []; // created a variable to holds the values that intersect
           /*Nested for loop that loops through the sub array items of strArr*/
           for(var i = 0; i < elem1.length; i++){
             //for loop loops through the first item of strArr
@@ -13,14 +13,13 @@
             //for loop loops through second item in strArr
             if(elem1[i] === elem2[j]){
               //if statement checks for items in the elem1 and elem2 that are equal
-              console.log(elem1[i],elem2[i]) //logs the equal items to the console
-            } else{
-              //runs when if condition is false
-             console.log(false)
+              intersectingNumbers.push(elem1[i],elem2[j])//logs the equal items to the console
             }
            }
           }
           elem1 = elem1.join(',')
           elem2 = elem2.join(',')
+          let result= intersectingNumbers.split(',')
+          return result
       }
-      console.log(findIntersection(['1,3,4,5,6','5,6,7,8,9']))
+      console.log(findIntersection(['1,3,4,16,0','5,6,7,8,9']))
